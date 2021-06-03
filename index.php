418a7346629c52bbffb25ca75a21e54ff080489f
SanRam01 <sramirez01097@misena.edu.co>
2021-06-03 18:26:16 -0500

    Cambios menores

--- index.php
+++ index.php
@@ -24,15 +24,15 @@
                 <a class="nav-link active" aria-current="page" href="#">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link" href="#">Registro</a>
               </li>
               <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Dropdown
                 </a>
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a class="dropdown-item" href="usuario/iniciarsesion.php">Iniciar Sesión</a></li>
-                  <li><a class="dropdown-item" href="usuario/registro.php">Registro</a></li>
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
                   <li><hr class="dropdown-divider"></li>
                   <li><a class="dropdown-item" href="#">Something else here</a></li>
                 </ul>
