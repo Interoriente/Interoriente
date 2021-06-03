4b2f6066ebba205256d3302e33d7a3e615bab0ca
SanRam01 <sramirez01097@misena.edu.co>
2021-06-03 18:57:05 -0500

    Actualización CRUD

--- principal/registro.php
+++ principal/registro.php
@@ -1,30 +1,3 @@
-<?php
-if ($_POST) {
-    //Llamar a la conexion base de datos
-    include_once '../dao/conexion.php';
-    //Capturo información
-    $correo = strip_tags($_POST['correo']);
-    $contrasena = strip_tags($_POST['contrasena']);
-    $contrasena = sha1($_POST['contrasena']);
-    $sql_correoexistente = "SELECT*FROM tblusuarios WHERE correo='$correo'";
-    $consulta_correo = $pdo->prepare($sql_correoexistente);
-    $consulta_correo->execute();
-    $resultado_correo = $consulta_correo->rowCount();
-    var_dump($resultado_correo);
-    if ($resultado_correo) {
-        echo "<script>alert('El correo ingresado ya existe!, por favor verificalo e intenta nuevamente');</script>";
-    } else {
-        //sentencia Sql
-        $sql_insertar = "INSERT INTO tblusuarios (correo,contrasena)VALUES (?,?)";
-        //Preparar consulta
-        $consulta_insertar = $pdo->prepare($sql_insertar);
-        //Ejecutar la sentencia
-        $consulta_insertar->execute(array($correo, $contrasena));
-        echo "<script>alert('Datos almacenados correctamente');</script>";
-        echo "<script> document.location.href='../usuario/iniciarsesion.php';</script>";
-    }
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -36,11 +9,11 @@ if ($_POST) {
 </head>
 
 <body>
-    <form action="" method="post">
+    <form action="../php/crud/registro.php" method="post">
         <label for="correo">Correo</label>
-        <input type="text" name="correo" id="">
+        <input type="text" name="correo" id=""><br><br>
         <label for="contrasena">Contraseña</label>
-        <input type="password" name="contrasena" id="">
+        <input type="password" name="contrasena" id=""><br><br>
         <button type="submit">Enviar</button>
     </form>
 </body>
