8b6ddb683606562f7d4909950886a7ab28f3f362
Santiago614 <sgomez9002@misena.edu.co>
2021-06-04 06:17:34 -0500

    Agregando los link de php para que sea más fácil hacer cambios en un archivo y que se vea en donde se está linkeando

---
+++ dashboard/dashPrin/examples/upgrade.php
@@ -0,0 +1,172 @@
+<!--
+=========================================================
+* Argon Dashboard - v1.2.0
+=========================================================
+* Product Page: https://www.creative-tim.com/product/argon-dashboard
+
+
+* Copyright  Creative Tim (http://www.creative-tim.com)
+* Coded by www.creative-tim.com
+
+
+
+=========================================================
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
+  <meta name="author" content="Creative Tim">
+  <title>Argon Dashboard - Free Dashboard for Bootstrap 4</title>
+  <!-- Favicon -->
+  <link rel="icon" href="../assets/img/brand/favicon.png" type="image/png">
+  <!-- Fonts -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
+  <!-- Icons -->
+  <link rel="stylesheet" href="../assets/vendor/nucleo/css/nucleo.css" type="text/css">
+  <link rel="stylesheet" href="../assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
+  <!-- Argon CSS -->
+  <link rel="stylesheet" href="../assets/css/argon.css?v=1.2.0" type="text/css">
+</head>
+
+<body>
+  <?php require_once '../assets/sidebar.php' ?>
+  <?php require_once '../assets/header.php' ?>
+  <!-- Header -->
+  <div class="header bg-primary pb-6">
+    <div class="container-fluid">
+      <div class="header-body">
+        <div class="row align-items-center py-4">
+          <div class="col-lg-6 col-7">
+            <h6 class="h2 text-white d-inline-block mb-0">Upgrade to PRO</h6>
+            <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
+              <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
+                <li class="breadcrumb-item"><a href="#"><i class="fas fa-home"></i></a></li>
+                <li class="breadcrumb-item"><a href="#">Examples</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Upgrade to pro</li>
+              </ol>
+            </nav>
+          </div>
+          <div class="col-lg-6 col-5 text-right">
+            <a href="#" class="btn btn-sm btn-neutral">New</a>
+            <a href="#" class="btn btn-sm btn-neutral">Filters</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Page content -->
+  <div class="container-fluid mt--6">
+    <div class="row mt--5">
+      <div class="col-md-10 ml-auto mr-auto">
+        <div class="card card-upgrade">
+          <div class="card-header text-center border-bottom-0">
+            <h4 class="card-title">Argon Dashboard PRO</h4>
+            <p class="card-category">Are you looking for more components? Please check our Premium Version of Argon Dashboard.</p>
+          </div>
+          <div class="card-body">
+            <div class="table-responsive table-upgrade">
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th class="text-center">Free</th>
+                    <th class="text-center">PRO</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Elements</td>
+                    <td class="text-center">100</td>
+                    <td class="text-center">200</td>
+                  </tr>
+                  <tr>
+                    <td>Plugins</td>
+                    <td class="text-center">4</td>
+                    <td class="text-center">16</td>
+                  </tr>
+                  <tr>
+                    <td>Example Pages</td>
+                    <td class="text-center">6</td>
+                    <td class="text-center">25</td>
+                  </tr>
+                  <tr>
+                    <td>DataTables, VectorMap, SweetAlert, Wizard,<br> jQueryValidation, FullCalendar etc...</td>
+                    <td class="text-center"><i class="ni ni-fat-remove text-danger"></i></td>
+                    <td class="text-center"><i class="ni ni-check-bold text-success"></i></td>
+                  </tr>
+                  <tr>
+                    <td>Mini Sidebar</td>
+                    <td class="text-center"><i class="ni ni-fat-remove text-danger"></i></td>
+                    <td class="text-center"><i class="ni ni-check-bold text-success"></i></td>
+                  </tr>
+                  <tr>
+                    <td>Premium Support</td>
+                    <td class="text-center"><i class="ni ni-fat-remove text-danger"></i></td>
+                    <td class="text-center"><i class="ni ni-check-bold text-success"></i></td>
+                  </tr>
+                  <tr>
+                    <td></td>
+                    <td class="text-center">Free</td>
+                    <td class="text-center">Just $79</td>
+                  </tr>
+                  <tr>
+                    <td class="text-center"></td>
+                    <td class="text-center">
+                      <a href="#" class="btn btn-round btn-default disabled">Current Version</a>
+                    </td>
+                    <td class="text-center">
+                      <a target="_blank" href="https://www.creative-tim.com/product/argon-dashboard-pro?ref=ad-update-pro" class="btn btn-round btn-primary">Upgrade to PRO</a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Footer -->
+    <footer class="footer pt-0">
+      <div class="row align-items-center justify-content-lg-between">
+        <div class="col-lg-6">
+          <div class="copyright text-center  text-lg-left  text-muted">
+            &copy; 2020 <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+            <li class="nav-item">
+              <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
+            </li>
+            <li class="nav-item">
+              <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">Blog</a>
+            </li>
+            <li class="nav-item">
+              <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md" class="nav-link" target="_blank">MIT License</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  </div>
+  </div>
+  <!-- Argon Scripts -->
+  <!-- Core -->
+  <script src="../assets/vendor/jquery/dist/jquery.min.js"></script>
+  <script src="../assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="../assets/vendor/js-cookie/js.cookie.js"></script>
+  <script src="../assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
+  <script src="../assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
+  <!-- Argon JS -->
+  <script src="../assets/js/argon.js?v=1.2.0"></script>
+</body>
+
+</html>
