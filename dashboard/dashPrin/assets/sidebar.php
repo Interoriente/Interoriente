6e5a1a4404a16e78b5653a3133773e066bc48df1
SanRam01 <sramirez01097@misena.edu.co>
2021-06-04 18:09:55 -0500

    Co-authored-by: Santiago614 <Santiago614@users.noreply.github.com> Co-authored-by: Gerard-glitch <Gerard-glitch@users.noreply.github.com>

--- dashboard/dashPrin/assets/sidebar.php
+++ dashboard/dashPrin/assets/sidebar.php
@@ -1,106 +1,73 @@
 <!-- Sidenav -->
 <nav class="sidenav navbar navbar-vertical  fixed-left  navbar-expand-xs navbar-light bg-white" id="sidenav-main">
-        <div class="scrollbar-inner">
-          <!-- Brand -->
-          <div class="sidenav-header  align-items-center">
-            <a class="navbar-brand" href="../../../index.php">
-              <img src="../../../assets/img/logo.svg" class="navbar-brand-img" alt="...">
+  <div class="scrollbar-inner">
+    <!-- Brand -->
+    <div class="sidenav-header  align-items-center">
+      <a class="navbar-brand" href="../../../index.php">
+        <img src="../../../assets/img/logo.svg" class="navbar-brand-img" alt="...">
+      </a>
+    </div>
+    <div class="navbar-inner">
+      <!-- Collapse -->
+      <div class="collapse navbar-collapse" id="sidenav-collapse-main">
+        <!-- Nav items -->
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="dashboard.php">
+              <i class="ni ni-planet text-orange"></i>
+              <span class="nav-link-text">Dashboard</span>
             </a>
-          </div>
-          <div class="navbar-inner">
-            <!-- Collapse -->
-            <div class="collapse navbar-collapse" id="sidenav-collapse-main">
-              <!-- Nav items -->
-              <ul class="navbar-nav">
-                <li class="nav-item">
-                  <a class="nav-link" href="dashboard.php">
-                    <i class="ni ni-planet text-orange"></i>
-                    <span class="nav-link-text">Dashboard</span>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="index.php">
-                    <i class="ni ni-pin-3 text-primary"></i>
-                    <span class="nav-link-text">Inicio</span>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="icons.php">
-                    <i class="ni ni-planet text-orange"></i>
-                    <span class="nav-link-text">Icons</span>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="map.php">
-                    <i class="ni ni-pin-3 text-primary"></i>
-                    <span class="nav-link-text">Google</span>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="profile.php">
-                    <i class="ni ni-single-02 text-yellow"></i>
-                    <span class="nav-link-text">Profile</span>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="tables.php">
-                    <i class="ni ni-bullet-list-67 text-default"></i>
-                    <span class="nav-link-text">Tables</span>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="login.php">
-                    <i class="ni ni-key-25 text-info"></i>
-                    <span class="nav-link-text">Login</span>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="register.php">
-                    <i class="ni ni-circle-08 text-pink"></i>
-                    <span class="nav-link-text">Register</span>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="upgrade.php">
-                    <i class="ni ni-send text-dark"></i>
-                    <span class="nav-link-text">Upgrade</span>
-                  </a>
-                </li>
-              </ul>
-              <!-- Divider -->
-              <hr class="my-3">
-              <!-- Heading -->
-              <h6 class="navbar-heading p-0 text-muted">
-                <span class="docs-normal">Documentation</span>
-              </h6>
-              <!-- Navigation -->
-              <ul class="navbar-nav mb-md-3">
-                <li class="nav-item">
-                  <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/getting-started/overview.html" target="_blank">
-                    <i class="ni ni-spaceship"></i>
-                    <span class="nav-link-text">Getting started</span>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/foundation/colors.html" target="_blank">
-                    <i class="ni ni-palette"></i>
-                    <span class="nav-link-text">Foundation</span>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/components/alerts.html" target="_blank">
-                    <i class="ni ni-ui-04"></i>
-                    <span class="nav-link-text">Components</span>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/plugins/charts.html" target="_blank">
-                    <i class="ni ni-chart-pie-35"></i>
-                    <span class="nav-link-text">Plugins</span>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </nav>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">
+              <i class="ni ni-pin-3 text-primary"></i>
+              <span class="nav-link-text">Inicio</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="icons.php">
+              <i class="ni ni-planet text-orange"></i>
+              <span class="nav-link-text">Icons</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="map.php">
+              <i class="ni ni-pin-3 text-primary"></i>
+              <span class="nav-link-text">Google</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="profile.php">
+              <i class="ni ni-single-02 text-yellow"></i>
+              <span class="nav-link-text">Profile</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="tables.php">
+              <i class="ni ni-bullet-list-67 text-default"></i>
+              <span class="nav-link-text">Tables</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="login.php">
+              <i class="ni ni-key-25 text-info"></i>
+              <span class="nav-link-text">Login</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="register.php">
+              <i class="ni ni-circle-08 text-pink"></i>
+              <span class="nav-link-text">Register</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="upgrade.php">
+              <i class="ni ni-send text-dark"></i>
+              <span class="nav-link-text">Upgrade</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
--- index.php
+++ index.php
@@ -25,44 +25,35 @@
 </div>
 
  <!--Carrusel-->
-    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-      <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-      </div>
-      <div class="carousel-inner" >
-        <div class="carousel-item active">
-          <img src="assets/img/1.jpg" class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>First slide label</h5>
-            <p>Some representative placeholder content for the first slide.</p>
-          </div>
-        </div>
-        <div class="carousel-item">
-          <img src="assets/img/2.jpeg" class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>Second slide label</h5>
-            <p>Some representative placeholder content for the second slide.</p>
-          </div>
-        </div>
-        <div class="carousel-item">
-          <img src="assets/img/3.jpeg" class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block">
-            <h5>Third slide label</h5>
-            <p>Some representative placeholder content for the third slide.</p>
-          </div>
-        </div>
-      </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
-    </div>
+ <div class="container">
+  
+  <ul class="slider">
+    <li id="slide1">
+      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3947459/car.jpg"/>
+    </li>
+    <li id="slide2">
+      <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3947459/sunset.jpg"/>
+    </li>
+    <li id="slide3">
+      <h1>Ejemplo con otros elementos</h1>
+      <p>Esto es un párrafo de ejemplo para comprobar que podemos meter cualquier tipo de elementos en el slider</p>
+      <a href="https://kikopalomares.com/">¡Corre a mi web para más contenido!</a>
+    </li>
+  </ul>
+  
+  <ul class="menu">
+    <li>
+      <a href="#slide1">1</a>
+    </li>
+    <li>
+      <a href="#slide2">2</a>
+    </li>
+     <li>
+      <a href="#slide3">3</a>
+    </li>
+  </ul>
+  
+</div>
 
   <!--Inicio Tarjetas-->
   <div class="tarjetasIndex">
