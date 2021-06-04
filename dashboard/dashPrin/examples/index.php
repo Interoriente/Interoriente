832f9f7088eecd2308038693305f368550526a89
Santiago614 <sgomez9002@misena.edu.co>
2021-06-04 06:38:56 -0500

    Mas correcciones

--- dashboard/dashPrin/examples/index.php
+++ dashboard/dashPrin/examples/index.php
@@ -30,7 +30,7 @@ if (isset($_SESSION["correo"]) or isset($_SESSION["idusuario"])) {
       <link rel="stylesheet" href="../assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
       <!-- Page plugins -->
       <!-- Argon CSS -->
-      <link rel="stylesheet" href="assets/css/argon.css?v=1.2.0" type="text/css">
+      <link rel="stylesheet" href="../assets/css/argon.css?v=1.2.0" type="text/css">
     </head>
 
     <body>
@@ -456,16 +456,16 @@ if (isset($_SESSION["correo"]) or isset($_SESSION["idusuario"])) {
       </div>
       <!-- Argon Scripts -->
       <!-- Core -->
-      <script src="assets/vendor/jquery/dist/jquery.min.js"></script>
-      <script src="assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-      <script src="assets/vendor/js-cookie/js.cookie.js"></script>
-      <script src="assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
-      <script src="assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
+      <script src="../assets/vendor/jquery/dist/jquery.min.js"></script>
+      <script src="../assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+      <script src="../assets/vendor/js-cookie/js.cookie.js"></script>
+      <script src="../assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
+      <script src="../assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
       <!-- Optional JS -->
-      <script src="assets/vendor/chart.js/dist/Chart.min.js"></script>
-      <script src="assets/vendor/chart.js/dist/Chart.extension.js"></script>
+      <script src="../assets/vendor/chart.js/dist/Chart.min.js"></script>
+      <script src="../assets/vendor/chart.js/dist/Chart.extension.js"></script>
       <!-- Argon JS -->
-      <script src="assets/js/argon.js?v=1.2.0"></script>
+      <script src="../assets/js/argon.js?v=1.2.0"></script>
     </body>
 
     </html>
