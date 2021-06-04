384ac822881eb6e052b40dd0ec06836bad8c5454
Santiago614 <sgomez9002@misena.edu.co>
2021-06-04 06:51:40 -0500

    cambios...

--- dashboard/dashPrin/assets/footer.php
+++ dashboard/dashPrin/assets/footer.php
@@ -4,22 +4,22 @@
       <div class="row align-items-center justify-content-lg-between">
         <div class="col-lg-6">
           <div class="copyright text-center  text-lg-left  text-muted">
-            &copy; 2020 <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a>
+            &copy; 2020 <a href="" class="font-weight-bold ml-1" target="_blank">Creative Tim</a>
           </div>
         </div>
         <div class="col-lg-6">
           <ul class="nav nav-footer justify-content-center justify-content-lg-end">
             <li class="nav-item">
-              <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
+              <a href="" class="nav-link" target="_blank">Interoriente</a>
             </li>
             <li class="nav-item">
-              <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">About Us</a>
+              <a href="" class="nav-link" target="_blank">Comprar</a>
             </li>
             <li class="nav-item">
-              <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">Blog</a>
+              <a href="" class="nav-link" target="_blank">Vender</a>
             </li>
             <li class="nav-item">
-              <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md" class="nav-link" target="_blank">MIT License</a>
+              <a href="" class="nav-link" target="_blank">Y mucho más</a>
             </li>
           </ul>
         </div>
