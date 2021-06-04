4e931f9cc960bc5f3afbb31da786b994b0d6fc1b
SanRam01 <sramirez01097@misena.edu.co>
2021-06-04 17:15:35 -0500

    Co-authored-by: Santiago614 <Santiago614@users.noreply.github.com>

---
+++ principal/registro.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrarse</title>
+</head>
+<body>
+    <form action="../php/crud/registro.php" method="POST">
+        <label for="nombre">Nombres </label>
+        <input type="text" name="nombres"><br><br>
+        <label for="apellido">Apellidos </label>
+        <input type="text" name="apellidos"><br><br>
+        <label for="telefono">Teléfono </label>
+        <input type="number" name="telefono"><br><br>
+        <label for="correo">Correo</label>
+        <input type="email" name="correo" id=""><br><br>
+        <label for="contrasena">Contraseña</label>
+        <input type="password" name="contrasena" id=""><br><br>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
