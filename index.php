1c13e9d7ba6d3034885ba632a27d697d8fdc2469
SanRam01 <sramirez01097@misena.edu.co>
2021-06-04 16:22:15 -0500

    Co-authored-by: SantiagoArismendy <SantiagoArismendy@users.noreply.github.com> Co-authored-by: Gerard-glitch <Gerard-glitch@users.noreply.github.com>

--- index.php
+++ index.php
@@ -5,73 +5,79 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">  
+  <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">
 
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
   <link rel="stylesheet" href="assets/bootstrap/css/estilos.css">
-  <title>Bienvenidos a InterOriente</title>
+  <title>Bienvenidos a Interoriente</title>
 </head>
 
 <body>
-
-
-
+  <!--Banner y logo-->
   <img id="banner" src="assets/img/banner_index.jpg" class="img-fluid" alt="...">
   <img id="logo" src="assets/img/Flor2.svg" class="rounded mx-auto d-block" alt="...">
+  <!--Carrusel-->
 
-
-
-  <div class="section" id="carousel">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-8 mr-auto ml-auto">
-
-          <div class="card card-raised card-carousel">
-            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" data-interval="3000">
-              <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1" class=""></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2" class=""></li>
-              </ol>
-              <div class="carousel-inner">
-                <div class="carousel-item active">
-                  <img class="d-block w-100" src="https://rawgit.com/creativetimofficial/material-kit/master/assets/img/bg.jpg" alt="First slide">
-                  <div class="carousel-caption d-none d-md-block">
-                    <h4>
-                      <i class="material-icons">location_on</i>
-                      Yellowstone National Park, United States
-                    </h4>
-                  </div>
-                </div>
-                <div class="carousel-item">
-                  <img class="d-block w-100" src="https://rawgit.com/creativetimofficial/material-kit/master/assets/img/bg2.jpg" alt="Second slide">
-                  <div class="carousel-caption d-none d-md-block">
-                    <h4>
-                      <i class="material-icons">location_on</i>
-                      Somewhere Beyond, United States
-                    </h4>
-                  </div>
-                </div>
-                <div class="carousel-item">
-                  <img class="d-block w-100" src="https://rawgit.com/creativetimofficial/material-kit/master/assets/img/bg3.jpg" alt="Third slide">
-                  <div class="carousel-caption d-none d-md-block">
-                    <h4>
-                      <i class="material-icons">location_on</i>
-                      Yellowstone National Park, United States
-                    </h4>
-                  </div>
-                </div>
-              </div>
-            </div>
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner" >
+        <div class="carousel-item active">
+          <img src="assets/img/1.jpg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>First slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
           </div>
-
         </div>
+        <div class="carousel-item">
+          <img src="assets/img/2.jpeg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Second slide label</h5>
+            <p>Some representative placeholder content for the second slide.</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="assets/img/3.jpeg" class="d-block w-100" alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Third slide label</h5>
+            <p>Some representative placeholder content for the third slide.</p>
+          </div>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+
+  <!--Inicio Tarjetas-->
+  <div class="tarjetasIndex">
+    <div class="card" id="tarjetaUno" style="width: 18rem;">
+      <img src="assets/img/1.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+
+    <div class="card" id="tarjetaDos" style="width: 18rem;">
+      <img src="assets/img/2.jpeg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
       </div>
     </div>
   </div>
-  </div>
-
 
 
   <footer class="footer">
@@ -82,27 +88,10 @@
   <script src="https://unpkg.com/popper.js@1.12.6/dist/umd/popper.js" integrity="sha384-fA23ZRQ3G/J53mElWqVJEGJzU0sTs+SvzG8fXVWP+kJQ1lwFAOkcUOysnlKJC33U" crossorigin="anonymous"></script>
   <script src="https://unpkg.com/bootstrap-material-design@4.1.1/dist/js/bootstrap-material-design.js" integrity="sha384-CauSuKpEqAFajSpkdjv3z9t8E7RlpJ1UP0lKM/+NdtSarroVKu069AlsRPKkFBz9" crossorigin="anonymous"></script>
   <div class="tituloindex">
-  <h1>Inicia sesión o regístrate para poder disfrutar de todos los beneficios de tener una cuenta con nosotros. </h1>
+    <h1 class="text">Inicia sesión o regístrate para poder disfrutar de todos los beneficios de tener una cuenta con nosotros. </h1>
   </div>
-  <div class="botonIndex">
-    <a href="principal/iniciarsesion.php" class="btn btn-white btn-animation-1">¡Vamos!</a>
-  </div>
-  <div class="card" id="tarjetaUno" style="width: 18rem;">
-    <img src="assets/img/1.jpg" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
-  </div>
-  <div class="card" id="tarjetaDos" style="width: 18rem;">
-    <img src="assets/img/2.jpeg" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
-  </div>
+
+
 
   <button type="button" class="btn btn-success">Success</button>
 
