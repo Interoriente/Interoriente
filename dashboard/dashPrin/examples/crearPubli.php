0df28bb68249ab61b668c0a2c1885cbdff368063
Santiago614 <sgomez9002@misena.edu.co>
2021-06-06 09:00:00 -0500

    Probando con otro codigo para ssubir imagenes al server

--- dashboard/dashPrin/examples/crearPubli.php
+++ dashboard/dashPrin/examples/crearPubli.php
@@ -133,30 +133,48 @@ if (isset($_SESSION["correo"]) or isset($_SESSION["idusuario"])) {
                 <input type="number" id="input-first-name" class="form-control" placeholder="Costo" name="costo" required>
 
                 <label>Imagen</label>
-                <input type="file" name="foto" value="file" required>
+                <input type="file" name="archivo" value="file" required>
 
                 <button type="submit" name="subir">Enviar</button>
                 <?php
                 if (isset($_POST['subir'])) {
                   include_once '../../../dao/conexion.php';
-                  var_dump($_FILES['foto']);
-                  $titulo = $_POST['titulo'];
-                  $descripcion = $_POST['descripcion'];
-                  $costo = $_POST['costo'];
-                  $foto = $_FILES["foto"]["name"];
-                  $ruta = $_FILES["foto"]["tmp_name"];
-                  $destino = "../../../imagenes/" . $foto;
-                  copy($ruta, $destino);
-                  //sentencia Sql
-                  $sql_insertar = "INSERT INTO tblPublicacion (nombrePublicacion,descripcion,costo ,imagen )VALUES (?,?,?,?)";
-                  //Preparar consulta
-                  $consulta_insertar = $pdo->prepare($sql_insertar);
-                  //Ejecutar la sentencia
-                  $consulta_insertar->execute(array($titulo, $descripcion, $costo, $destino));
-                  echo "<script>alert('El registro se subió correctamente');</script>";
-                  echo "<script> document.location.href='crearPubli.php';</script>";
+                  if (isset($_POST['subir'])) {
+                    //Recogemos el archivo enviado por el formulario
+                    $archivo = $_FILES['archivo']['name'];
+                    //Si el archivo contiene algo y es diferente de vacio
+                    if (isset($archivo) && $archivo != "") {
+                      //Obtenemos algunos datos necesarios sobre el archivo
+                      $tipo = $_FILES['archivo']['type'];
+                      $tamano = $_FILES['archivo']['size'];
+                      $temp = $_FILES['archivo']['tmp_name'];
+                      //Se comprueba si el archivo a cargar es correcto observando su extensión y tamaño
+                      if (!((strpos($tipo, "gif") || strpos($tipo, "jpeg") || strpos($tipo, "jpg") || strpos($tipo, "png")) && ($tamano < 2000000))) {
+                        echo '<div><b>Error. La extensión o el tamaño de los archivos no es correcta.<br/>
+                         - Se permiten archivos .gif, .jpg, .png. y de 200 kb como máximo.</b></div>';
+                      } else {
+                        //Si la imagen es correcta en tamaño y tipo
+                        //Se intenta subir al servidor
+                        if (move_uploaded_file($temp, 'images/' . $archivo)) {
+                          //Cambiamos los permisos del archivo a 777 para poder modificarlo posteriormente
+                          chmod('images/' . $archivo, 0777);
+                          //Almacenamiento en BD
+                          //sentencia Sql
+                          $sql_insertar = "INSERT INTO tblPublicacion (nombrePublicacion,descripcion,costo ,imagen )VALUES (?,?,?,?)";
+                          //Preparar consulta
+                          $consulta_insertar = $pdo->prepare($sql_insertar);
+                          //Ejecutar la sentencia
+                          $consulta_insertar->execute(array($titulo, $descripcion, $costo, $archivo));
+                          echo "<script>alert('El registro se subió correctamente');</script>";
+                          echo "<script> document.location.href='crearPubli.php';</script>";
+                        } else {
+                          //Si no se ha podido subir la imagen, mostramos un mensaje de error
+                          echo '<div><b>Ocurrió algún error al subir el fichero. No pudo guardarse.</b></div>';
+                        }
+                      }
+                    }
+                  }
                 }
-
                 ?>
                 <!-- Footer -->
                 <?php require_once '../assets/footer.php' ?>
