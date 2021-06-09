ec5d3657144bdc844d7567ef9113bcc9c0fd2736
SanRam01 <sramirez01097@misena.edu.co>
2021-06-09 17:41:55 -0500

    "

--- dashboard/dashPrin/examples/dashboard.php
+++ dashboard/dashPrin/examples/dashboard.php
@@ -14,7 +14,6 @@ if (isset($_SESSION["correo"]) or isset($_SESSION["idusuario"])) {
 ?>
     <!DOCTYPE html>
     <html>
-
     <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
