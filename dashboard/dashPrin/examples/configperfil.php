80e5617152e2028e3437224ea06ba0ca60157bd5
Santiago614 <sgomez9002@misena.edu.co>
2021-06-10 07:32:58 -0500

    Realizando pruebas

--- dashboard/dashPrin/examples/configperfil.php
+++ dashboard/dashPrin/examples/configperfil.php
@@ -46,21 +46,7 @@ if (isset($_SESSION["emailUsuario"]) or isset($_SESSION["documentoIdentidad"])) 
             //Validacion de roles
             if ($resultado) {
             ?>
-                <!-- Header -->
-                <div class="header pb-6 d-flex align-items-center" style="min-height: 500px; background-image: url(../assets/img/theme/profile-cover.jpg); background-size: cover; background-position: center top;">
-                    <!-- Mask -->
-                    <span class="mask bg-gradient-default opacity-8"></span>
-                    <!-- Header container -->
-                    <div class="container-fluid d-flex align-items-center">
-                        <div class="row">
-                            <div class="col-lg-7 col-md-10">
-                                <h1 class="display-2 text-white">Hola! <?php echo $resultado2->nombresUsuario; ?></h1>
-                                <a href="#!" class="btn btn-neutral">Editar perfil</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
+            <br>
                 <div class="col-xl-8 order-xl-1">
                     <div class="card">
                         <div class="card-header">
--- dashboard/dashPrin/examples/perfil.php
+++ dashboard/dashPrin/examples/perfil.php
@@ -35,143 +35,103 @@ if (isset($_SESSION["emailUsuario"]) or isset($_SESSION["documentoIdentidad"])) 
     <body>
       <?php require_once '../assets/sidebar.php' ?>
       <?php require_once '../assets/header.php' ?>
-      <!-- Header -->
-      <div class="header pb-6 d-flex align-items-center" style="min-height: 500px; background-image: url(../assets/img/theme/profile-cover.jpg); background-size: cover; background-position: center top;">
-        <!-- Mask -->
-        <span class="mask bg-gradient-default opacity-8"></span>
-        <!-- Header container -->
-        <div class="container-fluid d-flex align-items-center">
-          <div class="row">
-            <div class="col-lg-7 col-md-10">
-              <h1 class="display-2 text-white">Hello Jesse</h1>
-              <p class="text-white mt-0 mb-5">This is your profile page. You can see the progress you've made with your work and manage your projects or assigned tasks</p>
-              <a href="#!" class="btn btn-neutral">Edit profile</a>
+      <?php
+      $id = $_SESSION["emailUsuario"];
+      include_once '../../../dao/conexion.php';
+      $sql_inicio = "SELECT*FROM tblUsuario WHERE emailUsuario ='$id' ";
+      $consulta_resta = $pdo->prepare($sql_inicio);
+      $consulta_resta->execute();
+      $resultado = $consulta_resta->rowCount(array($id));
+      $resultado2 = $consulta_resta->fetch(PDO::FETCH_OBJ);
+      //Validacion de roles
+      if ($resultado) {
+      ?>
+        <!-- Header -->
+        <div class="header pb-6 d-flex align-items-center" style="min-height: 500px; background-image: url(../assets/img/theme/profile-cover.jpg); background-size: cover; background-position: center top;">
+          <!-- Mask -->
+          <span class="mask bg-gradient-default opacity-8"></span>
+          <!-- Header container -->
+          <div class="container-fluid d-flex align-items-center">
+            <div class="row">
+              <div class="col-lg-7 col-md-10">
+                <h1 class="display-2 text-white">Hola! <?php echo $resultado2->nombresUsuario; ?></h1>
+                <a href="configperfil.php" class="btn btn-neutral">Editar perfil</a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <!-- Page content -->
-      <div class="container-fluid mt--6">
-        <div class="row">
-          <div class="col-xl-4 order-xl-2">
-            <div class="card card-profile">
-              <img src="../assets/img/theme/img-1-1000x600.jpg" alt="Image placeholder" class="card-img-top">
-              <div class="row justify-content-center">
-                <div class="col-lg-3 order-lg-2">
-                  <div class="card-profile-image">
-                    <a href="#">
-                      <img src="../assets/img/theme/team-4.jpg" class="rounded-circle">
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div class="card-header text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
-                <div class="d-flex justify-content-between">
-                  <a href="#" class="btn btn-sm btn-info  mr-4 ">Connect</a>
-                  <a href="#" class="btn btn-sm btn-default float-right">Message</a>
-                </div>
-              </div>
-              <div class="card-body pt-0">
-                <div class="row">
-                  <div class="col">
-                    <div class="card-profile-stats d-flex justify-content-center">
-                      <div>
-                        <span class="heading">22</span>
-                        <span class="description">Friends</span>
-                      </div>
-                      <div>
-                        <span class="heading">10</span>
-                        <span class="description">Photos</span>
-                      </div>
-                      <div>
-                        <span class="heading">89</span>
-                        <span class="description">Comments</span>
-                      </div>
+
+        <!-- Page content -->
+        <div class="container-fluid mt--6">
+          <div class="row">
+            <div class="col-xl-4 order-xl-2">
+              <div class="card card-profile">
+                <img src="../assets/img/theme/img-1-1000x600.jpg" alt="Image placeholder" class="card-img-top">
+                <div class="row justify-content-center">
+                  <div class="col-lg-3 order-lg-2">
+                    <div class="card-profile-image">
+                      <a href="#">
+                        <img src="../assets/img/theme/team-4.jpg" class="rounded-circle">
+                      </a>
                     </div>
                   </div>
                 </div>
-                <div class="text-center">
-                  <h5 class="h3">
-                    Jessica Jones<span class="font-weight-light">, 27</span>
-                  </h5>
-                  <div class="h5 font-weight-300">
-                    <i class="ni location_pin mr-2"></i>Bucharest, Romania
-                  </div>
-                  <div class="h5 mt-4">
-                    <i class="ni business_briefcase-24 mr-2"></i>Solution Manager - Creative Tim Officer
-                  </div>
-                  <div>
-                    <i class="ni education_hat mr-2"></i>University of Computer Science
+                <div class="card-header text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
+                  <div class="d-flex justify-content-between">
+                    <a href="#" class="btn btn-sm btn-info  mr-4 ">Connect</a>
+                    <a href="#" class="btn btn-sm btn-default float-right">Message</a>
                   </div>
                 </div>
-              </div>
-            </div>
-          </div>
-          <?php
-          $id = $_SESSION["emailUsuario"];
-          include_once '../../../dao/conexion.php';
-          $sql_inicio = "SELECT*FROM tblUsuario WHERE emailUsuario ='$id' ";
-          $consulta_resta = $pdo->prepare($sql_inicio);
-          $consulta_resta->execute();
-          $resultado = $consulta_resta->rowCount(array($id));
-          $resultado2 = $consulta_resta->fetch(PDO::FETCH_OBJ);
-          //Validacion de roles
-          if ($resultado) {
-          ?>
-            <div class="col-xl-8 order-xl-1">
-              <div class="card">
-                <div class="card-header">
-                  <div class="row align-items-center">
-                    <div class="col-8">
-                      <h3 class="mb-0">Editar perfil </h3>
-                    </div>
-                  </div>
-                </div>
-                <div class="card-body">
-                  <form action="crud/actualizarCuenta.php" method="GET">
-                    <h6 class="heading-small text-muted mb-4">Información de usuario</h6>
-                    <div class="pl-lg-4">
-                      <div class="row">
-                        <div class="col-lg-6">
-                          <div class="form-group">
-                            <label class="form-control-label" for="input-username">Nombre</label>
-                            <input type="text" id="input-username" name="nombre" class="form-control" placeholder="Username" value="<?php echo $resultado2->nombresUsuario; ?>">
-                          </div>
-                          <div class="form-group">
-                            <label class="form-control-label" for="input-username">Apellido</label>
-                            <input type="text" id="input-username" name="apellido" class="form-control" placeholder="Username" value="<?php echo $resultado2->apellidoUsuario; ?>">
-                          </div>
+                <div class="card-body pt-0">
+                  <div class="row">
+                    <div class="col">
+                      <div class="card-profile-stats d-flex justify-content-center">
+                        <div>
+                          <span class="heading">22</span>
+                          <span class="description">Friends</span>
                         </div>
-                        <p>Ten en cuenta que si modificas el correo deberás iniciar sesión nuevamente</p>
-                        <div class="col-lg-6">
-                          <div class="form-group">
-                            <label class="form-control-label" for="input-email">Correo</label>
-                            <input type="email" id="input-email" name="correo" class="form-control" value="<?php echo $resultado2->emailUsuario; ?>">
-                          </div>
-                          <div class="form-group">
-                            <input type="hidden" name="ideditar" value="<?php echo $resultado2->documentoIdentidad; ?>">
-                          </div>
+                        <div>
+                          <span class="heading">10</span>
+                          <span class="description">Photos</span>
+                        </div>
+                        <div>
+                          <span class="heading">89</span>
+                          <span class="description">Comments</span>
                         </div>
                       </div>
-                      <button class="btn btn-primary btn-xs" type="submit" name="subir">Editar</button>
                     </div>
-                  </form>
+                  </div>
+                  <div class="text-center">
+                    <h5 class="h3">
+                      Jessica Jones<span class="font-weight-light">, 27</span>
+                    </h5>
+                    <div class="h5 font-weight-300">
+                      <i class="ni location_pin mr-2"></i>Bucharest, Romania
+                    </div>
+                    <div class="h5 mt-4">
+                      <i class="ni business_briefcase-24 mr-2"></i>Solution Manager - Creative Tim Officer
+                    </div>
+                    <div>
+                      <i class="ni education_hat mr-2"></i>University of Computer Science
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           <?php } ?>
           <!-- Footer -->
-          <?php require_once '../assets/footer.php' ?>
         </div>
-        <!-- Argon Scripts -->
-        <!-- Core -->
-        <script src="../assets/vendor/jquery/dist/jquery.min.js"></script>
-        <script src="../assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-        <script src="../assets/vendor/js-cookie/js.cookie.js"></script>
-        <script src="../assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
-        <script src="../assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
-        <!-- Argon JS -->
-        <script src="../assets/js/argon.js?v=1.2.0"></script>
+          <?php require_once '../assets/footer.php' ?>
+          
+          <!-- Argon Scripts -->
+          <!-- Core -->
+          <script src="../assets/vendor/jquery/dist/jquery.min.js"></script>
+          <script src="../assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+          <script src="../assets/vendor/js-cookie/js.cookie.js"></script>
+          <script src="../assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
+          <script src="../assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
+          <!-- Argon JS -->
+          <script src="../assets/js/argon.js?v=1.2.0"></script>
     </body>
 
     </html>
