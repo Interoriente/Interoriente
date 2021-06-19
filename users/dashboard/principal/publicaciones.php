b9f88e83dc0dcca53fad5cd25dd1e488a414658a
Santiago614 <sgomez9002@misena.edu.co>
2021-06-19 11:30:28 -0500

    Verificacion error detectado

--- users/dashboard/principal/publicaciones.php
+++ users/dashboard/principal/publicaciones.php
@@ -95,13 +95,15 @@ if (isset($_SESSION["emailUsuario"]) or isset($_SESSION["documentoIdentidad"])) 
                     /* foreach ($resultado_mostrar_publi as $datos) {
                       foreach ($resultadoEstado as $datos1) {  */
                     //Sirve para especificar que solo se imprima si se cumple la condición, y lo necesito por el motivo de que estoy llamando a tblPublicacion y a tblEstado, entonces necesito imprimir a los dos en una misma tabla.
-                    while ($resultado_mostrar_publi = $consultar_mostrar_publi->fetch(PDO::FETCH_OBJ)) {
+                    while ($resultado_mostrar_publi = $consultar_mostrar_publi->fetch(PDO::FETCH_OBJ) AND $resultadoEstado  = $consultaMostrarEstado->fetch(PDO::FETCH_OBJ)) {
                     ?>
                       <tr>
                         <th><?php echo $resultado_mostrar_publi->nombrePublicacion; ?></th>
                         <th><?php echo $resultado_mostrar_publi->descripcionPublicacion; ?></th>
                         <th><?php echo $resultado_mostrar_publi->costoPublicacion; ?></th>
                         <th><?php echo $resultado_mostrar_publi->stockProducto; ?></th>
+                        <!-- Tener en cuenta que el estado está saliendo trocado 1->2 2->1:( -->
+                        <th><?php echo $resultadoEstado->nombreEstado; ?></th>
                       </tr>
                     <?php
                       /*   }
