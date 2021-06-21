1fc9a4eed32c518373f3260e8b314b7123743d1d
SanRam01 <sramirez01097@misena.edu.co>
2021-06-21 13:54:38 -0500

    Co-authored-by: Gerard-glitch <Gerard-glitch@users.noreply.github.com>

---
+++ principal/navegacion/avisoVerificacion.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/css/estilosVerificacion.css">
+    <link rel="stylesheet" href="../../assets/css/estilosNavs.css">
+    <title>Verficacion</title>
+</head>
+
+<body>
+    <!-- Barra de navegación -->
+
+
+    <div class="contenedor-prin">
+        <div class="contenedor-principal">
+            <div class="">
+                <a href="">Preguntas Frecuentes</a>
+            </div>
+            <div class="">
+                <a href="">test 3</a>
+            </div>
+            <div class="">
+                <a href="">test 2</a>
+            </div>
+            <div class="">
+                <a href="">Test </a>
+            </div>
+            <div class="">
+                <a href="">Ayuda</a>
+            </div>
+
+
+
+
+        </div>
+    </div>
+
+</body>
+
+</html>
