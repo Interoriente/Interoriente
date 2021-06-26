daafbe2d41c0dbe63955b675cb41d99cef661579
SanRam01 <sramirez01097@misena.edu.co>
2021-06-26 16:53:49 -0500

    Index 2.0

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -8,13 +8,13 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
   }
-  /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+  /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
   }
   
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
--- principal/navegacion/index.php
+++ principal/navegacion/index.php
@@ -8,7 +8,7 @@
     <link rel="icon" href="../../assets/img/favicon.png" type="image/png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../assets/css/indexEstilos.css">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous" />
     <title>Interoriente | Explora</title>
 </head>
 
@@ -20,19 +20,60 @@
                 <img src="../../assets/img/index/index.svg" alt="">
             </div>
         </div>
-    </section>
-
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ab? Commodi vel facere soluta, doloribus obcaecati dolores laboriosam sit deserunt natus voluptatibus reiciendis, provident aperiam! Autem quidem quisquam repudiandae hic?</p>
-    <section class="btns-filtrado">
-        <div class="contendedor-btns">
-            <div class="btn-productos">
-                <a href="">Test</a>
-            </div>
-            <div class="btn-servicios">
-                <a href="">Teasdst</a>
+        <div class="carrusel-container">
+            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="../../assets/img/index_2/1.jpg" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>First slide label</h5>
+                            <p>Some representative placeholder content for the first slide.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="../../assets/img/index_2/2.jpg" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Second slide label</h5>
+                            <p>Some representative placeholder content for the second slide.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="../../assets/img/index_2/3.jpg" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Third slide label</h5>
+                            <p>Some representative placeholder content for the third slide.</p>
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
             </div>
         </div>
     </section>
+
+    <h1 class="titulo-filtrado">Aquí podrás filtrar resultados </h1>
+    <section class="btns-filtrado">
+        <div class="btn-productos">
+            <a href="">Productos</a>
+        </div>
+        <div class="btn-servicios">
+            <a href="">Servicios</a>
+        </div>
+
+    </section>
+    
+    
 
 
 
@@ -48,7 +89,7 @@
             <a href="#contact">Ayuda</a>
         </div>
     </div>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </body>
 
 </html>
