0c83461bd5403ec005f5a511731a3280f569b088
Gerard-glitch <83957879+Gerard-glitch@users.noreply.github.com>
2021-07-14 22:43:10 -0500

    Vistas terminadas con éxito PHP

---
+++ principal/navegacion/catalogo.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/estiCatalogo.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <title>Catálogo</title>
+</head>
+<body background="assets/img/fondocatalogo">
+    <div class="padre">
+        <div class="header">
+            <nav class="nav">
+                <div class="tit-nav-caja">
+                    <h1 class="tit-nav">INTER ORIENTE</h1>
+                </div>
+            </nav>
+            <div class="contenedor-menu">
+                <div class="menuuno"><a href="#">
+                    <i class="fas fa-box-open"></i>
+                    <h1 class="tit-menu">Productos</h1>
+                    <p class="par-menu">Ejemplo: Libros...</p>  
+                </a>    
+                </div>
+            
+                <div class="menudos"><a href="#">
+                    <i class="fas fa-box-open"></i>
+                    <h1 class="tit-menu">Servicios</h1>
+                    <p class="par-menu">Ejemplo: Reparación...</p>  
+                </a>    
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
