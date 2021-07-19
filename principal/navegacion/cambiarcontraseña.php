afa2fd4997a82f5c595f351e9e605562218df637
Gerard-glitch <83957879+Gerard-glitch@users.noreply.github.com>
2021-07-19 12:35:41 -0500

    Update cambiarcontraseña.php

--- principal/navegacion/cambiarcontraseña.php
+++ principal/navegacion/cambiarcontraseña.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/estiCambiarContra.css">
+    <link rel="stylesheet" href="../../assets/css/estiCambiarContra.css">
     <title>Cambiar Contraseña</title>
 </head>
 <body background="../../assets/img/fondocambiarcontrasena.jpg">
