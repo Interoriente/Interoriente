b5ad9731b9ed24a2d8aae960bbb997788a2b3d2d
Santiago614 <sgomez9002@misena.edu.co>
2021-07-28 08:25:06 -0500

    Corrigiendo detalles en los link de los sidebar agregados

--- principal/navegacion/opcionesEnvio.php
+++ principal/navegacion/opcionesEnvio.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../assets/css/estiOpcionesEnvio.css">
     <link rel="icon" href="../../assets/img/favicon.png" type="image/png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous" />
     <title>Opviones de Envío</title>
 </head>
 
@@ -46,6 +47,8 @@
     <?php
     include '../includes/navMovil.php';
     ?>
+    <!-- Js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- principal/navegacion/recucontrasena.php
+++ principal/navegacion/recucontrasena.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../assets/css/estiRecuperarContraseña.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous" />
     <link rel="icon" href="../../assets/img/favicon.png" type="image/png" />
     <title>Recuperar Contraseña</title>
 </head>
@@ -21,11 +22,6 @@
 
     <div class="padre">
         <div class="header">
-            <nav class="nav">
-                <div class="tit-nav-caja">
-                    <h1 class="tit-nav">INTER ORIENTE</h1>
-                </div>
-            </nav>
             <div class="menu1">
                 <h1 class="tit-menu1">RECUPERA TU CONTRASEÑA</h1>
                 <div class="caja-formulario">
@@ -39,14 +35,18 @@
                     </p>
                 </div>
             </div>
-            <!-- Sidebar -->
-            <?php include '../includes/navInferior.php'; ?>
-            <!-- ------- -->
+        </div>
+    </div>
+    <!-- Sidebar -->
+    <?php include '../includes/navInferior.php'; ?>
+    <!-- ------- -->
 
-            <!-- Barra de navegación para dispositivos móviles -->
-            <?php
-            include '../includes/navMovil.php';
-            ?>
+    <!-- Barra de navegación para dispositivos móviles -->
+    <?php
+    include '../includes/navMovil.php';
+    ?>
+    <!-- Js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </body>
 
 </html>
