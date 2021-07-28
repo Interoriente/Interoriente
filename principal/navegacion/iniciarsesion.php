7ad23894ee58b258f8a120b2fec7f24c4d0571dd
Santiago614 <sgomez9002@misena.edu.co>
2021-07-28 18:35:26 -0500

    Organizando ciertos formularios

--- principal/navegacion/iniciarsesion.php
+++ principal/navegacion/iniciarsesion.php
@@ -83,7 +83,7 @@
         <div class="container mt--8 pb-5">
             <!-- Table -->
             <div class="row justify-content-center">
-                <div class="col-lg-5 col-md-7">
+                <div class="col-lg-6 col-md-7">
                     <div class="card bg-secondary border-0">
                         <div class="card-header bg-transparent pb-5">
                             <div class="text-muted text-center mt-2 mb-4"><small>Puedes iniciar sesión por:</small></div>
@@ -100,9 +100,9 @@
                         </div>
                         <div class="card-body px-lg-5 py-lg-5">
                             <div class="text-center text-muted mb-4">
-                                <small>O con tus datos puedes diligenciar tus datos en los siguientes campos</small>
+                                <small>O con tus datos personales:</small>
                             </div>
-                            <form action="../../php/crud/iniciarSesion.php" method="POST" id="formulario">
+                            <form action="../../php/crud/iniciarSesion.php" method="POST">
                                 <div class="form-group">
                                     <label for="id">Correo o documento:</label>
                                     <div class="input-group input-group-merge input-group-alternative mb-3">
@@ -118,7 +118,7 @@
                                         <div class="input-group-prepend">
                                             <span class="input-group-text"><i class="ni ni-key-25"></i></span>
                                         </div>
-                                        <input class="form-control" id="contrasena" placeholder="**********" type="password" name="contrasena" minlength="8" maxlength="20" required>
+                                        <input class="form-control" id="contrasena" placeholder="**********" type="password" name="contrasena" minlength="2" maxlength="20" required>
                                     </div>
                                 </div>
                                 <div class="text-center">
