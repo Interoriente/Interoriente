a74aa911de346a9a5a24fe1d10aba876036044ad
Gerard-glitch <83957879+Gerard-glitch@users.noreply.github.com>
2021-07-30 01:25:16 -0500

    Vista "Mis Compras" en HTML

--- principal/navegacion/miscompras.php
+++ principal/navegacion/miscompras.php
@@ -1,18 +1,135 @@
 <?php session_start();
 ?>
 <!DOCTYPE html>
+<<<<<<< HEAD
+<html lang="en">
+=======
 <html lang="es">
+>>>>>>> 30e8782a7ab7a31ca7c4bdae4c2e704dce9aa00e
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<<<<<<< HEAD
+    <link rel="stylesheet" href="../../assets/css/estilosMisCompras.css">
+    <link rel="icon" href="../../assets/img/favicon.png" type="image/png">
+    <title>Mis Compras</title>
+=======
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous" />
     <link rel="icon" href="../../assets/img/favicon.png" type="image/png" />
     <title>Document</title>
+>>>>>>> 30e8782a7ab7a31ca7c4bdae4c2e704dce9aa00e
 </head>
 
 <body>
+<<<<<<< HEAD
+
+<?php
+    include '../includes/superior.php';
+    ?>  
+
+<div class="padre">
+        <div class="header">
+            <div class="contenedores-menu">
+                <div class="menu-menu">
+            <div class="menu">
+                <div class="cont-menuuno">
+                <img class="imagen-cont" src="../../assets/img/computador.jpg" alt="">
+                </div>
+                <div class="cont-menudos">
+                    <p><strong>Computador</strong></p>
+                    <p>$2.323.000</p>
+                    <p>18/05/2021</p>
+                    <p>HP</p>
+                </div>
+                <div class="cont-menutres">
+                    <button class="boton-menu">Volver a comprar</button>
+                    <button class="boton-menu">Hacer un reclamo</button>
+                </div>
+            </div>
+            <div class="menu">
+            <div class="cont-menuuno">
+                <img class="imagen-cont" src="../../assets/img/cajas.jpg" alt="">
+                </div>
+                <div class="cont-menudos">
+                    <p><strong>Cajas</strong></p>
+                    <p>$80.000</p>
+                    <p>18/05/2021</p>
+                    <p>Cajas S.A.</p>
+                </div>
+                <div class="cont-menutres">
+                    <button class="boton-menu">Volver a comprar</button>
+                    <button class="boton-menu">Hacer un reclamo</button>
+                </div>
+            </div>
+            <div class="menu">
+            <div class="cont-menuuno">
+                <img class="imagen-cont" src="../../assets/img/manorobot.jpg" alt="">
+                </div>
+                <div class="cont-menudos">
+                    <p><strong>Mano Robótica</strong></p>
+                    <p>$200.000</p>
+                    <p>28/09/2021</p>
+                    <p>Mano</p>
+                </div>
+                <div class="cont-menutres">
+                    <button class="boton-menu">Volver a comprar</button>
+                    <button class="boton-menu">Hacer un reclamo</button>
+                </div>
+            </div>
+            <div class="menu">
+            <div class="cont-menuuno">
+                <img class="imagen-cont" src="../../assets/img/php.png" alt="">
+                </div>
+                <div class="cont-menudos">
+                    <p><strong>Curso PHP</strong></p>
+                    <p>$30.000</p>
+                    <p>20/01/2021</p>
+                    <p>PHP</p>
+                </div>
+                <div class="cont-menutres">
+                    <button class="boton-menu">Volver a comprar</button>
+                    <button class="boton-menu">Hacer un reclamo</button>
+                </div>
+            </div>
+            <div class="menu">
+            <div class="cont-menuuno">
+                <img class="imagen-cont" src="../../assets/img/banderaingles.jpg" alt="">
+                </div>
+                <div class="cont-menudos">
+                    <p><strong>Curso Inglés</strong></p>
+                    <p>$60.000</p>
+                    <p>8/10/2021</p>
+                    <p>Curso S.A.</p>
+                </div>
+                <div class="cont-menutres">
+                    <button class="boton-menu">Volver a comprar</button>
+                    <button class="boton-menu">Hacer un reclamo</button>
+                </div>
+            </div>
+            <div class="menu">
+            <div class="cont-menuuno">
+                <img class="imagen-cont" src="../../assets/img/iarobots.jpg" alt="">
+                </div>
+                <div class="cont-menudos">
+                    <p><strong>Curso IA</strong></p>
+                    <p>$56.000</p>
+                    <p>11/11/2021</p>
+                    <p>Curso S.A.</p>
+                </div>
+                <div class="cont-menutres">
+                    <button class="boton-menu">Volver a comprar</button>
+                    <button class="boton-menu">Hacer un reclamo</button>
+                    </div>
+                    </div>
+                </div>
+            </div>   
+            </div>
+        </div>
+    </div>
+    <?php include '../includes/navInferior.php'; ?>
+=======
     <!-- Barra de navegación -->
     <?php
     include '../includes/superior.php';
@@ -30,6 +147,7 @@
     ?>
     <!-- Js -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+>>>>>>> 30e8782a7ab7a31ca7c4bdae4c2e704dce9aa00e
 </body>
 
 </html>
