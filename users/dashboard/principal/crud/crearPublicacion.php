3146b050559b35b7c3b43ffed8ec245e64590446
Santiago614 <sgomez9002@misena.edu.co>
2021-08-20 11:41:12 -0500

    Prueba

--- users/dashboard/principal/crud/crearPublicacion.php
+++ users/dashboard/principal/crud/crearPublicacion.php
@@ -3,7 +3,6 @@ if (isset($_FILES['imagen'])) {
     /* Almacenando información del formulario crear publicacion */
     include_once '../../../../dao/conexion.php';
     $nombre = $_POST['nombre'];
-    echo $nombre;
     $descripcion = $_POST['descripcion'];
     $color = $_POST['color'];
     $costo = $_POST['costo'];
@@ -18,49 +17,46 @@ if (isset($_FILES['imagen'])) {
     VALUES (?,?,?,?,?,?,?,?,?)";
     //Preparar consulta
     $consulta_insertar = $pdo->prepare($sql_insertar);
-    $consulta_insertar->execute(array($nombre, $usuario, $descripcion, $color, $costo, $estadoarticulo, $stock, $categoria, $verificacion));
+    if ($consulta_insertar->execute(array($nombre, $usuario, $descripcion, $color, $costo, $estadoarticulo, $stock, $categoria, $verificacion))) {
+        echo "<script>alert('Registro almacenado');</script>";
+    }
+    
     //Seleccionar último id en la BD
     $datos = ($pdo->lastInsertId());
     $idPubli = "id" . $datos . " ";
     //Crear carpetas
     /* if (mkdir($idPubli, 0777, true)) {
         } */
-    if (isset($_FILES['imagen'])) {
-
-        $cantidad = count($_FILES["imagen"]["tmp_name"]);
-        //Ejecutar la sentencia
-        for ($i = 0; $i < $cantidad; $i++) {
-            //Comprobamos si el fichero es una imagen
-            if ($_FILES['imagen']['type'][$i] == 'image/png' || $_FILES['imagen']['type'][$i] == 'image/jpeg' || $_FILES['imagen']['type'][$i] == 'image/jpg') {
-                //Le defino una ruta a la imagen
-                $directorio = "../../../../imagenesPubli/$idPubli";
-                //Nombre de la imagen
-                $filename = $_FILES['imagen']['name'][$i];
-                //Nombre temporal de la imagen -> Por defecto se necesita este paso.
-                $temporal = $_FILES['imagen']['tmp_name'][$i];
-                $ruta = $directorio . "" . $filename;
-                //Especifico el nombre que se va a guardar en la BD
-                $archivo = $idPubli . "" . $filename;
-                //Subimos la imagen al servidor
-                if (move_uploaded_file($temporal, $ruta)) {
-                    //Insertando imagen en la tabla
-                    $sqlInsertarImagen = "INSERT INTO tblImagenes (urlImagen,publicacionImagen)
+    $cantidad = count($_FILES["imagen"]["tmp_name"]);
+    //Ejecutar la sentencia
+    for ($i = 0; $i < $cantidad; $i++) {
+        //Comprobamos si el fichero es una imagen
+        if ($_FILES['imagen']['type'][$i] == 'image/png' || $_FILES['imagen']['type'][$i] == 'image/jpeg' || $_FILES['imagen']['type'][$i] == 'image/jpg') {
+            //Le defino una ruta a la imagen
+            $directorio = "../../../../imagenesPubli/$idPubli";
+            //Nombre de la imagen
+            $filename = $_FILES['imagen']['name'][$i];
+            //Nombre temporal de la imagen -> Por defecto se necesita este paso.
+            $temporal = $_FILES['imagen']['tmp_name'][$i];
+            $ruta = $directorio . "" . $filename;
+            //Especifico el nombre que se va a guardar en la BD
+            $archivo = $idPubli . "" . $filename;
+            //Subimos la imagen al servidor
+            if (move_uploaded_file($temporal, $ruta)) {
+                //Insertando imagen en la tabla
+                $sqlInsertarImagen = "INSERT INTO tblImagenes (urlImagen,publicacionImagen)
                     VALUES (?,?)";
-                    $consultaInsertar = $pdo->prepare($sqlInsertarImagen);
-                }
-                if ($consultaInsertar->execute(array($archivo, $datos))) {
-                    echo "<script>alert('El registro se subió correctamente');</script>";
-                    /* Redirigir después de almacenar la información */
-                    echo "<script> document.location.href='../crearPubli.php';</script>";
-                }
-            } else {
-                echo "<script>alert('Error: el archivo no es una imagen');</scrip>";
+                $consultaInsertar = $pdo->prepare($sqlInsertarImagen);
+            }
+            if ($consultaInsertar->execute(array($archivo, $datos))) {
+                echo "<script>alert('El registro se subió correctamente');</>";
+                /* Redirigir después de almacenar la información */
                 echo "<script> document.location.href='../crearPubli.php';</script>";
             }
+        } else {
+            echo "<script>alert('Error: el archivo no es una imagen');</scrip>";
+            echo "<script> document.location.href='../crearPubli.php';</script>";
         }
-    } else {
-        echo "<script>alert('Ocurrió un error');</script>";
-        echo "<script> document.location.href='../crearPubli.php';</script>";
     }
 } else {
     echo "<script>alert('Error!, no se ha iniciado sesión');</script>";
