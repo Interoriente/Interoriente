983067d8afd1c481f383023ac9af96793183b15d
SanRam01 <sramirez01097@misena.edu.co>
2021-08-21 15:11:00 -0500

    ""

--- dao/conexion.php
+++ dao/conexion.php
@@ -2,15 +2,13 @@
 //Configurar zona horaria de Colombia
 date_default_timezone_set('America/Bogota');
 
-$nombrehost="localhost";
+$nombrehost= "190.90.160.12"; //Dirección IP del host "Shared IP Address"
 $nombreBD="interori_interoriente";
 $host = "mysql:host=$nombrehost;dbname=$nombreBD";
 
 $usuario = "interori_interori";
 $contrasena = "B4O#ugJ]C#%,4";
 
-/* $usuario = "root";
-$contrasena = ""; */
 
 
 try {
