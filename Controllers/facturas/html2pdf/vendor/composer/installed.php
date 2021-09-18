4f6aa33f13c0b5260e0cb28b2aae19f4aa581979
Santiago614 <sgomez9002@misena.edu.co>
2021-09-18 11:52:31 -0500

    Probando cambiar de libreria para crear pdf version 1.0

---
+++ Controllers/facturas/html2pdf/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'a4c189741bf93ec0c71bf6cb0897ac32683300d0',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'a4c189741bf93ec0c71bf6cb0897ac32683300d0',
+            'dev_requirement' => false,
+        ),
+        'spipu/html2pdf' => array(
+            'pretty_version' => 'v4.6.1',
+            'version' => '4.6.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../spipu/html2pdf',
+            'aliases' => array(),
+            'reference' => '4898439e170ebf032513b8d3a83f0f5ffe85fece',
+            'dev_requirement' => false,
+        ),
+        'tecnickcom/tcpdf' => array(
+            'pretty_version' => '6.2.26',
+            'version' => '6.2.26.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../tecnickcom/tcpdf',
+            'aliases' => array(),
+            'reference' => '367241059ca166e3a76490f4448c284e0a161f15',
+            'dev_requirement' => false,
+        ),
+    ),
+);
