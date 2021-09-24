6bdd978b0dbe7756ce57ef84f038f0f037e857c3
santiagoramirez <santiagodandaramirez@gmail.com>
2021-09-24 07:39:52 -0500

    Elimando scripts

--- Views/navegacion/iniciarsesion.php
+++ Views/navegacion/iniciarsesion.php
@@ -117,13 +117,8 @@ require "../assets/linksGoogle.php";
     </div>
     <!-- Core -->
     <!-- <script src="../../Controller/js/formulario.js"></script> -->
-    <script src="../dashboard/assets/vendor/jquery/dist/jquery.min.js"></script>
-    <script src="../dashboard/assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="../dashboard/assets/vendor/js-cookie/js.cookie.js"></script>
-    <script src="../dashboard/assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
-    <script src="../dashboard/assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
+    
     <!-- Argon JS -->
     <script src="../../view/dashboard/Controller/js/argon.js?v=1.2.0"></script>
-  
 
 </html>
