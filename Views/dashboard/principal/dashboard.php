83f2ec0aada490c3a357473214a82b33393384ff
santiagoramirez <santiagodandaramirez@gmail.com>
2021-09-24 15:28:05 -0500

    Consultas proveedor
    Co-authored-by: Santiago614 <Santiago614@users.noreply.github.com>

--- Views/dashboard/principal/dashboard.php
+++ Views/dashboard/principal/dashboard.php
@@ -127,17 +127,18 @@ if (isset($respUserData)) {
                     <span class="text-nowrap">Since last month</span>
                   </p>
                 </div>
-
               </div>
             </div>
             <div class="col-xl-3 col-md-6">
               <div class="card card-stats">
                 <!-- Tarjeta -->
+                
+                <!-- Tarjeta para el admin -->
                 <div class="card-body">
                   <div class="row">
                     <div class="col">
-                      <h5 class="card-title text-uppercase text-muted mb-0">New users</h5>
-                      <span class="h2 font-weight-bold mb-0">2,356</span>
+                      <h5 class="card-title text-uppercase text-muted mb-0">Usuarios Registrados</h5>
+                      <span class="h2 font-weight-bold mb-0">1053</span>
                     </div>
                     <div class="col-auto">
                       <div class="icon icon-shape bg-gradient-orange text-white rounded-circle shadow">
@@ -158,7 +159,7 @@ if (isset($respUserData)) {
                 <div class="card-body">
                   <div class="row">
                     <div class="col">
-                      <h5 class="card-title text-uppercase text-muted mb-0">Sales</h5>
+                      <h5 class="card-title text-uppercase text-muted mb-0">Ventas</h5>
                       <span class="h2 font-weight-bold mb-0">924</span>
                     </div>
                     <div class="col-auto">
