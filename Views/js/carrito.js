affd85c53b0593bbca306d088fcb565d2e69ae10
santiagoramirez <santiagodandaramirez@gmail.com>
2021-10-22 13:49:17 -0500

    Mejorando Carrito

--- Views/js/carrito.js
+++ Views/js/carrito.js
@@ -65,22 +65,21 @@ function cerrarCarrito() {
 
 /* Función principal del carrito AJAX-JQUERY*/
 function addCarrito(id) {
-
   publicacionLocalStorage = Storage.getPublicacion();
   /* Obteniendo id de la publicación para hacer consulta a la bd */
   if (publicacionLocalStorage) {
     for (let i = 0; i < publicacionLocalStorage.length; i++) {
       if (publicacionLocalStorage[i].id === id) {
         inputCantidad = document.querySelectorAll(".cantidad-items"); //Seleccionando todos los elementos con esa clase
-     
+
         for (let i = 0; i < inputCantidad.length; i++) {
           if (inputCantidad[i].id === id) {
             inputExistente = inputCantidad[i];
             break;
           }
         }
-       
-        newVlr = (parseInt(inputExistente.value) + 1);
+
+        newVlr = parseInt(inputExistente.value) + 1;
         inputExistente.value = newVlr;
         cambiarCantidad(id);
         abrirCarrito();
@@ -116,8 +115,6 @@ function getPublicacionDb(id) {
   });
 }
 
-
-
 /* Crear clase para mostrar elementos en el carrito */
 
 function renderPubli(item) {
@@ -128,7 +125,7 @@ function renderPubli(item) {
                 <img src="${item.img}" alt="product" width="100%">
                 <div>
                     <h4 class="titulos item-h" >${item.titulo}</h4>
-                    <h5>$${item.costo}</h5>
+                    <h5>$${number_format(item.costo)}</h5>
                     <span class="remove-item" onclick = "removeItem(this.id)" id = "${item.id}">Eliminar</span>
                 </div>
                 <div>
@@ -165,7 +162,6 @@ function cambiarCantidad(idItem) {
   /* Recorriendo el arreglo en busca del item con el mismo id */
   carrito.map((item) => {
     if (item.id === id) {
-  
       item.cantidad = parseInt(inputId.value);
     }
   });
@@ -197,10 +193,33 @@ function mathCarrito(item) {
     totalTmp += costo * item.cantidad;
     totalItems += item.cantidad;
   });
-  carritoTotal.textContent = parseFloat(totalTmp.toFixed(2));
+  carritoTotal.textContent = number_format(parseFloat(totalTmp), 3, '', '.');
   cartItems.textContent = totalItems;
 }
 
+function number_format (number, decimals, dec_point, thousands_sep) {
+  // Strip all characters but numerical ones.
+  number = (number + '').replace(/[^0-9+\-Ee.]/g, '');
+  let n = !isFinite(+number) ? 0 : +number,
+      prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+      sep = (typeof thousands_sep === 'undefined') ? '.' : thousands_sep,
+      dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+      s = '',
+      toFixedFix = function (n, prec) {
+          let k = Math.pow(10, prec);
+          return '' + Math.round(n * k) / k;
+      };
+  // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+  s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+  if (s[0].length > 3) {
+      s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+  }
+  if ((s[1] || '').length < prec) {
+      s[1] = s[1] || '';
+      s[1] += new Array(prec - s[1].length + 1).join('');
+  }
+  return s.join(dec);
+}
 /* Finalizar Compra */
 finCompra.addEventListener("click", function () {
   let carrito = Storage.getPublicacion();
