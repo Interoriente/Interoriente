dfad54e5200b311526a6b02aa78c1b3079fdac7f
SantiagoArismendy <82401067+SantiagoArismendy@users.noreply.github.com>
2021-11-22 18:55:40 -0500

    Update recuperar.php

--- Views/navegacion/phpmailer/recuperar.php
+++ Views/navegacion/phpmailer/recuperar.php
@@ -23,7 +23,7 @@ try {
     // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     
     $mail->SMTPDebug = 0;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
+    //$mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'kevinarismendy21@gmail.com';                     //SMTP username
