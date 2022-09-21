c0365f2f203a5c444654e8201e9a7f62c1f6742b
Santiago614 <sgomez9002@misena.edu.co>
2022-09-21 18:03:53 -0500

    Usuario - conexión

--- Models/dao/conexion.php
+++ Models/dao/conexion.php
@@ -1,11 +1,11 @@
 <?php
 //Configurar zona horaria de Colombia
 date_default_timezone_set('America/Bogota');
-$nombrehost = "localhost"; //Dirección IP del host "Shared IP Address"
+$nombrehost = "190.90.160.12"; //Dirección IP del host "Shared IP Address"
 $nombreBD = "interori_interoriente";
 $host = "mysql:host=$nombrehost;dbname=$nombreBD";
-$usuario = "root";
-$contrasena = "";
+$usuario = "interori_interori";
+$contrasena = "DB)(sFvTcbYv";
 try {
      //Conexion exitosa	
      $pdo = new PDO($host, $usuario, $contrasena);
